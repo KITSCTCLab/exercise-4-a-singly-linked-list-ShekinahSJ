6893b6d726958a548c8c703ce8ad0ec16ddb91e0
ShekinahSJ <100009660+ShekinahSJ@users.noreply.github.com>
2022-08-11 20:14:04 +0530

    Eighth commit

--- Main.py
+++ Main.py
@@ -3,11 +3,15 @@ from typing import Optional
 
 class Node:
     """
-    Provide necessary documentation
+    This class describes Node objects to act as elements of the LinkedList
+    Attributes:
+        -> data - stored associated data
+        -> next - link to next node
     """
+
     def __init__(self, data=None, next=None):
         """
-        Provide necessary documentation
+        Initialises the Node with given attributes
         """
         self.data = data
         self.next = next
@@ -15,8 +19,14 @@ class Node:
 
 class LinkedList:
     """
-    Provide necessary documentation
+    This class implements LinkedList using Node objects
+    Methods:
+        -> insert_at_end - inserts node with data at the end of the list
+        -> status - displays all elements of the lisT
+    Attributes
+        -> self.head - contains first node of LinkedList, None if list empty
     """
+
     def __init__(self):
         """
         Initialize the head
@@ -28,32 +38,33 @@ class LinkedList:
         Insert node at end of the list
         :param data: integer data that will be used to create a node
         """
-        # Write code here
-        newNode=Node(data)
-        if self.head is None:
-            self.head=newNode
+        new = Node(data, None)
+        current = self.head
+        if current is None:
+            self.head = new
         else:
-            temp=self.head
-            while (temp.next is not None):
-                temp=temp.next
-            temp.next=newNode
+            while current.next is not None:
+                current = current.next
+            current.next = new
 
     def status(self):
         """
         It prints all the elements of list.
         """
-        # write code here
-        displaylist=self.head
-        while displaylist!=None:
-            print(displaylist.next)
-            displaylist=displaylist.next
-        
+        elements = []
+        current = self.head
+        while current:
+            elements.append(current.data)
+            current = current.next
+        print(elements)
 
 
 class Solution:
     """
-    Provide necessary documentation
+    Class implementing functions to add numbers in a LinkedListT
+    
     """
+
     def addTwoNumbers(self, first_list: Optional[LinkedList], second_list: Optional[LinkedList]) -> Optional[
         LinkedList]:
         """
@@ -81,10 +92,11 @@ class Solution:
             curr = curr.next
         return int(num)
 
-# Do not edit the following code      
+
+# Do not edit the following code
 # Create an instance for LinkedList
 first_list = LinkedList()
-# Create an another instance for LinkedList
+# Create an another instance for LinkedListT
 second_list = LinkedList()
 # Read data for first list
 data_for_first_list = list(map(int, input().strip().split(" ")))
